4f36018d0e8bc3b8dd7aaca2216bcc29a79f1a1c
Gustavo <gusfrabatraba@gmail.com>
2019-03-14 09:14:08 -0500

    modulos

---
+++ src/app/pages/pages.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { Graficas1Component } from './graficas1/graficas1.component';
+import { ProgressComponent } from './progress/progress.component';
+import { PagesComponent } from './pages.component';
+import { SharedModule } from '../shared/shared.module';
+
+@NgModule({
+  declarations: [
+    PagesComponent,
+    DashboardComponent,
+    ProgressComponent,
+    Graficas1Component,
+  ],
+  exports: [
+    DashboardComponent,
+    ProgressComponent,
+    Graficas1Component,
+  ],
+  imports: [
+    CommonModule,
+    SharedModule
+  ]
+})
+export class PagesModule { }
---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { BreadcumbsComponent } from './breadcumbs/breadcumbs.component';
+import { HeaderComponent } from './header/header.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+
+@NgModule({
+  declarations: [
+    BreadcumbsComponent,
+    HeaderComponent,
+    SidebarComponent
+  ],
+  exports:[
+    BreadcumbsComponent,
+    HeaderComponent,
+    SidebarComponent
+  ],
+  imports: [
+    CommonModule,
+  ]
+})
+export class SharedModule { }
